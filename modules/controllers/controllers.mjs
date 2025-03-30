686d08def1339487b5e38490612baca2e4ba5f5b
Raff_12 <raffelecoppola381@gmail.com>
2025-03-30 23:41:27 +0200

    feat: Aggiunta dei parametri in più

--- modules/controllers/controllers.mjs
+++ modules/controllers/controllers.mjs
@@ -5,9 +5,40 @@ import express from "express"
 import posts from "../posts.mjs"
 
 const ShowPosts = (req, res) =>{
-    return res.status(201).json({msg: "Benvenuto nella lista dei Post:", lista: posts});
+    return res.status(201).json({msg: "Benvenuto nella lista dei Post:", posts: posts});
+}
+
+const IdPostsSearch = (req, res) => {
+    const Number_index = parseInt(String(req.params.id).slice(-1));
+    // console.log(Number_index);
+    if (isNaN(Number_index)) {
+        return res.send(401).json({ msg: "Il Parametro non è un numero, mi dispiace."});
+    }
+    const finder_posts = posts.find( post => post.id === Number_index);
+
+    if (!finder_posts) {
+        return res.status(404).json({ msg: "Post non è stato trovato"});
+    }
+
+    // console.log(finder_posts);
+    return res.status(201).json({ msg: "Ecco il Post che hai cercato:", post: finder_posts});
+}
+
+const QueryPostsTagsSearch = (req, res) =>{
+    // console.log(req.query);
+    const { query: {tags, tags2} } = req;
+    // console.log(tags, tags2);
+    const tags_vl = [tags, tags2];
+    let tags_posts_finder = posts.filter( post => { return tags_vl.some( tag => post.tags.includes(tag)) });
+    // console.log(tags_posts_finder);
+    if (tags_posts_finder.length === 0) {
+        return res.status(401).json({ msg: "I post con i tag usati non esistono"});
+    }
+    return res.status(201).json({ msg: "Ecco i post trovati con i tag:", posts: tags_posts_finder });
 }
 
 export {
     ShowPosts,
+    IdPostsSearch,
+    QueryPostsTagsSearch
 }
--- modules/routes.mjs
+++ modules/routes.mjs
@@ -2,12 +2,21 @@
 // Creazione delle rotte del sito in questione
 
 import express from "express"
-import { ShowPosts } from "./controllers/controllers.mjs";
+import { ShowPosts , IdPostsSearch, QueryPostsTagsSearch} from "./controllers/controllers.mjs";
+import posts from "./posts.mjs";
 
 const routes = express.Router();
 
 // Prima Rotta:
 
-routes.get("/posts", ShowPosts);
+routes.get("/posts/all", ShowPosts);
+
+// Seconda Rotta:
+
+routes.get("/posts/:id", IdPostsSearch);
+
+// Terza Rotta:
+
+routes.get("/posts", QueryPostsTagsSearch)
 
 export default routes
